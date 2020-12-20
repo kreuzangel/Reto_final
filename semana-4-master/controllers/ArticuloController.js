634958363f0b0d89eee793f594096ecb699cf290
kreuzangel <lichtdenschatten@gmail.com>
2020-12-20 14:27:29 -0500

    bien

--- semana-4-master/controllers/ArticuloController.js
+++ semana-4-master/controllers/ArticuloController.js
@@ -1,5 +1,6 @@
+const { request } = require('express');
 const { Articulo } = require('../models/');
-
+const articulo = require('../models/');
 
 
 module.exports = {
--- semana-4-master/controllers/CategoriaController.js
+++ semana-4-master/controllers/CategoriaController.js
@@ -1,6 +1,5 @@
 const { Categoria } = require('../models/');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+
 
 
 module.exports = {
