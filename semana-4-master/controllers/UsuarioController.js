06d0d3bbcb35ffe96213720e38d7d1aa6ba9904b
kreuzangel <lichtdenschatten@gmail.com>
2020-12-22 16:21:27 -0500

    18 pruebas ok

--- semana-4-master/controllers/UsuarioController.js
+++ semana-4-master/controllers/UsuarioController.js
@@ -6,29 +6,62 @@ const jwt = require('jsonwebtoken');
 
 
 module.exports = {
-    list : async (req,res,next) =>{
+    list: async (req, res, next) => {
         try {
-    const re =await Usuario.findAll()
+            const re = await Usuario.findAll()
 
-    res.status(200).json(re)
-        } catch (error) { 
+            res.status(200).json(re)
+        } catch (error) {
             res.status(500).json({ 'error': 'Oops paso algo que mal' })
             next(error)
-        } 
+        }
     },
-    add : async (req,res,next) =>{
-            try {
-                req.body.password = bcrypt.hashSync(req.body.password, 10)
-                const re = await Usuario.create(req.body)
+    add: async (req, res, next) => {
+        try {
+            req.body.password = bcrypt.hashSync(req.body.password, 10)
+            const re = await Usuario.create(req.body)
 
-                res.status(200).json(re)
-            } catch (error) {
-                res.status(500).json({ 'error': 'Oops paso algo' })
-                next(error)
-            }
+            res.status(200).json(re)
+        } catch (error) {
+            res.status(500).json({ 'error': 'Oops paso algo' })
+            next(error)
+        }
     },
-    register : async (req,res,next) =>{
-        res.status(200).send('se hará en el sprint 3')
+    update: async (req, res, next) => {
+
+        try {
+            const user = await Usuario.findOne({ where: { id: req.body.id } })
+            const validPassword = bcrypt.compareSync(req.body.password, user.password)
+            if (validPassword) {
+ //               const newEncriptedPassword = bcrypt.hashSync(req.body.newpassword)
+                const re = await Usuario.update({ nombre: req.body.nombre, email: req.body.email }, { where: { id: req.body.id } })
+                res.status(200).send(re)
+            }
+
+        } catch (error) {
+
+        }
+    },
+    activate: async (req, res, next) => {
+        try {
+            const re = await Usuario.update({ estado: 1 }, { where: { id: req.body.id } })
+            res.status(200).json(re)
+
+        } catch (error) {
+            res.status(500).json({ 'error': 'Oops paso algo' })
+            next(error)
+        }
+    },
+
+    deactivate: async (req, res, next) => {
+        try {
+
+            const re = await Usuario.update({ estado: 0 }, { where: { id: req.body.id } })
+            res.status(200).json(re)
+        } catch (error) {
+            res.status(500).json({ 'error': 'Oops paso algo' })
+            next(error)
+        }
     },
     login : async (req, res, next) => {
 
@@ -37,25 +70,25 @@ module.exports = {
 
             if (user) {
                 // Evaluar contraseña
-                const contrasenhaValida= bcrypt.compareSync(req.body.password, user.password)
+                const contrasenhaValida = bcrypt.compareSync(req.body.password, user.password)
 
-                if (contrasenhaValida) 
+                if (contrasenhaValida) {
 
-                {
+                    const token = servToken.encode(user.id, user.rol)
 
-                   const token = servToken.encode(user.id,user.rol)
-                    
                     res.status(200).send({
-                        auth:  true,  
+                        auth: true,
                         tokenReturn: token,
                         user: user
                     })
 
 
                 } else {
-                    
-                    res.status(401).json({ auth: false , email : user.password, tokenReturn: null , reason:
-                        "Invalid Password!" });
+
+                    res.status(401).json({
+                        auth: false, email: user.password, tokenReturn: null, reason:
+                            "Invalid Password!"
+                    });
                 }
 
             } else {
--- semana-4-master/routes/usuario.js
+++ semana-4-master/routes/usuario.js
@@ -8,6 +8,9 @@ const router = routerx();
 router.get('/list', usuarioController.list)
 router.post('/add', usuarioController.add)
 router.post('/login', usuarioController.login)
+router.put('/update', usuarioController.update)
+router.put('/activate', usuarioController.activate)
+router.put('/deactivate', usuarioController.deactivate)
 
 
 module.exports = router;
